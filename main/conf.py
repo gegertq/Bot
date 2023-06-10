016119bbdac236a019b498e0af610b654b3d2d51
gegertq <107989413+gegertq@users.noreply.github.com>
2023-06-10 10:38:43 +0300

    Update conf.py

--- main/conf.py
+++ main/conf.py
@@ -1,1 +1,20 @@
-
+close_options = ['Да', 'Нет']
+questions = [
+    {'text': 'Выходите ли вы из себя по малейшему поводу?', 'qtype': 'closed', 'options': close_options},
+    {'text': 'Боитесь ли вы разозлить человека, который заведомо физически сильнее вас?', 'qtype': 'closed',
+     'options': close_options},
+    {'text': 'Начинаете ли вы скандалить, чтобы на вас обратили внимание?', 'qtype': 'closed',
+     'options': close_options},
+    {'text': 'Любите ли вы ездить на большой скорости, даже если это связано с риском для жизни?', 'qtype': 'closed',
+     'options': close_options},
+    {'text': 'Увлекаетесь ли вы лекарствами, когда заболеете?', 'qtype': 'closed', 'options': close_options},
+    {'text': 'Пойдете ли вы на все, чтобы получить то, что вам очень хочется?', 'qtype': 'closed',
+     'options': close_options},
+    {'text': 'Любите ли вы больших собак?', 'qtype': 'closed', 'options': close_options},
+    {'text': 'Любите ли вы сидеть часами на солнце?', 'qtype': 'closed', 'options': close_options},
+    {'text': 'Уверены ли вы, что когда-нибудь станете знаменитостью?', 'qtype': 'closed', 'options': close_options},
+    {'text': 'Умеете ли вы вовремя остановиться, если чувствуете, что начинаете проигрывать?', 'qtype': 'closed',
+     'options': close_options},
+    {'text': 'Привыкли ли вы много есть, даже если не голодны?', 'qtype': 'closed', 'options': close_options},
+    {'text': 'Любите ли вы знать заранее, что вам подарят?', 'qtype': 'closed', 'options': close_options}
+]
